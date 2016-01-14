20b68f6e5a0cc4e5e35ef3cdca044aa15529e3e9
dewei <zhangdewei@guihua.com>
2016-01-14 21:18:58 +0800

    update

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: server
 server:
-	browser-sync start --server --files='index.html,bundle/app.css'
+	browser-sync start --server --files='index.html,bundle/app.css,js/app.js'
 
 .PHONY: css
 	mkdir -p bundle
---
+++ js/app.js
@@ -0,0 +1,51 @@
+window.onload = function(){
+
+	var doc = document;
+
+	function animateLogo(){
+		TweenMax.to(
+			".logo-img", 2, {
+				css: {
+					y:'20px',
+				},
+				repeat: -1,
+				yoyo: true,
+				ease: Power2.easeInOut
+			}
+		);  // animate finish;
+	}; // function finish;
+
+	animateLogo();
+
+	function animateRobote(){
+		t = new TimelineMax({yoyo: true, repeat:-1});
+		t.to("#android-robot", 2, {rotation: '20deg'})
+		 .to("#android-robot", 2, {rotation: '-20deg'})
+
+	};  // function finish;
+
+	animateRobote();
+
+	function updateSliderControl(){
+		var links = doc.querySelectorAll('#slider-control a');
+
+		for(var i=0,n=links.length;i<n;i++){
+			var link = doc.querySelector(links[i].getAttribute('href'));
+
+			var sectionTop = link.offsetTop;
+			var sectionBottom = sectionTop + link.offsetHeight;
+		
+			if(window.scrollY > sectionTop && window.scrollY < sectionBottom){
+				link.className = 'active';
+			}else{
+				link.className = '';
+			}
+
+		};
+
+	};  // function finish;
+
+
+	updateSliderControl();
+
+};
